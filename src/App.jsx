a90ebe2ba0ef97dd75e972e297275ff44df45fc5
AEGcarlosfigueroa <carlos.figueroa@ikasle.aeg.eus>
2025-12-17 09:00:08 +0100

    [UPDATE] page header in index.html, new contact info

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import logo from './assets/logo.png'
 import './App.css'
 import memberInfo from './info/memberInfo.json'
@@ -7,6 +6,7 @@ import Service from './props/service'
 import mailLogo from './assets/mail.png'
 import phoneLogo from './assets/phone.png'
 import serviceInfo from './info/serviceInfo.json'
+import homeLogo from "./assets/homelogo.png"
 
 function App() {
 
@@ -40,11 +40,15 @@ function App() {
           <h2 style={{textAlign: 'left', marginLeft: '5%', color: 'black'}}>CONTACTO</h2>
           <div style={{display: 'flex', flexDirection: 'row'}}>
             <img src={mailLogo} style={{height: '5%', resize: 'vertical', width: '4%', alignSelf: 'left', marginLeft: '2%', marginBottom: '2%'}}/>
-            <h3 style={{color: 'black', marginLeft: '2%'}}>empresadivinasdelamuerte@gmail.com</h3>
+            <h3 style={{color: 'black', marginLeft: '2%'}}>Divinas@gmail.ws</h3>
           </div>
           <div style={{display: 'flex', flexDirection: 'row'}}>
             <img src={phoneLogo} style={{height: '5%', resize: 'vertical', width: '4%', alignSelf: 'left', marginLeft: '2%', marginBottom: '2%'}}/>
-            <h3 style={{color: 'black', marginLeft: '2%'}}>+34614939267</h3>
+            <h3 style={{color: 'black', marginLeft: '2%'}}>943078899</h3>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'row'}}>
+            <img src={homeLogo} style={{height: '5%', resize: 'vertical', width: '4%', alignSelf: 'left', marginLeft: '2%', marginBottom: '2%'}}/>
+            <h3 style={{color: 'black', marginLeft: '2%'}}>Narrika kalea, 27, 20001, Donostia / San Sebastian, Gipuzkua</h3>
           </div>
         </div>
       </div>
